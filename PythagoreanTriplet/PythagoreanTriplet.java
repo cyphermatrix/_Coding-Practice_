1176305214aa39741c1294b6c017da68f9b40373
cyphermatrix <andy_a_wang@yahoo.com>
2016-06-21 13:04:51 -0500

    Update PythagoreanTriplet.java

--- PythagoreanTriplet/PythagoreanTriplet.java
+++ PythagoreanTriplet/PythagoreanTriplet.java
@@ -1,1 +1,12 @@
 //PythagoreanTriplet 
+public class PyTrip {
+  public static void main(String[] args)
+  {
+    int a = args[1];
+    int b = args[2];
+    int c = args[3];
+    if(a * a + b * b == c * c)
+      System.out.println("true");
+    System.out.println("false");
+  }
+}
