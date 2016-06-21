9aa97f8d2d6ac3d741b1c67cfb4240a10bd5aa0e
cyphermatrix <andy_a_wang@yahoo.com>
2016-06-21 12:56:49 -0500

    Update Palindrome.java

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,1 +1,24 @@
 //Palindrome 
+public class Palin {
+  public static void main(String[] args)
+  {
+    if(isPalin)
+    System.out.println("True");
+    System.out.println("False");
+  }
+  public boolean isPalin(int a)
+  {
+    int num = a;
+    int reverse = 0;
+    int digit = 0;
+    while(num > 0)
+    {
+      digit = num % 10;
+      reverse = reverse * 10 + dig;
+      num = num/10;
+    }
+    if(num == reverse)
+    return true;
+    return false;
+  }
+}
